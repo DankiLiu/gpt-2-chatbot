9001d43d7b863fb9c0ce6582fa37988f2728d29f
liudanqing <liudanqing1996@hotmail.com>
2022-05-09 11:15:20 +0200

    add print functions

--- model.py
+++ model.py
@@ -138,6 +138,7 @@ def load_model(file="saved_models/model8"):
 def decode(history, model):
     input_ids, token_type_ids, _ = build_training_data(history, "")
     output_ids = model(input_ids)
+    print(output_ids)
     """
     beam_outputs = model.generate(input_ids,
                                   max_length=50,
@@ -156,9 +157,12 @@ def decode(history, model):
     if int(num) not in range(len(beam_outputs)+1):
         num = input("Please select a response from the above sentences: ")
     """
-    output_ids = tokenizer.convert_ids_to_tokens(output_ids)
-    output = " ".join(output_ids)
-    return output
+
+    response_tokens = tokenizer.convert_ids_to_tokens(output_ids)
+    print(response_tokens)
+    response = " ".join(output_ids)
+    print(response)
+    return response
 
 
 def evaluate_model():
