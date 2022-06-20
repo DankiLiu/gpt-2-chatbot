a907c1e5516f7f96530f1f8cd586f3a17564baa7
liudanqing <liudanqing1996@hotmail.com>
2022-06-20 16:39:13 +0200

    delete some imports

--- pl_demo/lightning_demo.py
+++ pl_demo/lightning_demo.py
@@ -1,8 +1,6 @@
 import os
 import torch
 from torch import nn
-import torch.nn.functional as F
-from torchvision import transforms
 from torchvision.datasets import MNIST
 from torch.utils.data import DataLoader, random_split
 from pytorch_lightning.loggers import TensorBoardLogger
